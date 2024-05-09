6063302e685a4b93bb34b20b4c426c97b42c31c0
frandosis <frandosis@hotmail.com>
2024-05-09 15:31:26 +0200

    Correct insertion algorithm to apply swap at each eval.

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,15 +1,16 @@
 list_of_elements = [8, 1, 6, 2, 4, 3]
 
-def insertion_sort(list_: list)
+def insertion_sort(list_: list):
 	count = len(list_)
 	if count < 2: return
-	for i in range(1, count-1)
-		tmp = list_.pop(i)
+	for i in range(1, count):
+		key = list_[i]
 		j = i-1
-		while tmp <= list_[j]:
-			j--
-			if j == 0: break
-		list_.insert(j,tmp)
+		while j >= 0 and key < list_[j]:
+			list_[j+1] = list_[j]
+			j -= 1
+		list_[j+1] = key
+
 
 insertion_sort(list_of_elements)
 print(list_of_elements)
