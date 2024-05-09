ee69a0eb44b7b41936bada3d3d85b20ca314cc9e
frandosis <frandosis@hotmail.com>
2024-05-09 15:34:34 +0200

    Display each step in the insertion sort algorithm.

--- insertion_sort.py
+++ insertion_sort.py
@@ -4,13 +4,13 @@ def insertion_sort(list_: list):
 	count = len(list_)
 	if count < 2: return
 	for i in range(1, count):
+		print(list_)
 		key = list_[i]
 		j = i-1
 		while j >= 0 and key < list_[j]:
 			list_[j+1] = list_[j]
 			j -= 1
 		list_[j+1] = key
-
+	print(list_)
 
 insertion_sort(list_of_elements)
-print(list_of_elements)
