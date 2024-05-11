563edc512bdd4f5b1117fc5004dafbc2f066fd70
frandosis <frandosis@hotmail.com>
2024-05-11 15:19:03 +0200

    Add typing to the methods in AdjacencyMatrix.

--- src/datastructures/non_linear_datastructures/graph/adjacency_matrix.py
+++ src/datastructures/non_linear_datastructures/graph/adjacency_matrix.py
@@ -6,17 +6,17 @@ We can also make an edge a set of indexes for the matrix.
 
 
 class AdjacencyMatrix:
-    def __init__(self, total_vertices):
+    def __init__(self, total_vertices: int):
         self.total_vertices = total_vertices
         self.matrix = [[0] * total_vertices for _ in range(total_vertices)]
 
-    def add_edge(self, vertex1, vertex2):
+    def add_edge(self, vertex1: int, vertex2: int):
         if vertex1 == vertex2:
             print("Same vertex %d and %d" % (vertex1, vertex2))
         self.matrix[vertex1][vertex2] = 1
         self.matrix[vertex2][vertex1] = 1
 
-    def remove_edge(self, vertex1, vertex2):
+    def remove_edge(self, vertex1: int, vertex2: int):
         if self.matrix[vertex1][vertex2] == 0:
             print("No edge between %d and %d" % (vertex1, vertex2))
             return
