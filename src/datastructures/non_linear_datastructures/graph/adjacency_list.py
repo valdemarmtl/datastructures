d530f8ae2bafbc96a0aadcc94208626cae20d0da
frandosis <frandosis@hotmail.com>
2024-05-11 15:51:44 +0200

    Add directed and undirected insert of edges.

--- src/datastructures/non_linear_datastructures/graph/adjacency_list.py
+++ src/datastructures/non_linear_datastructures/graph/adjacency_list.py
@@ -4,3 +4,15 @@ class AdjacencyList:
     def __init__(self, total_vertices: int):
         self.total_vertices = total_vertices
         self.list_ = [[] for _ in range(total_vertices)]
+
+    def add_directed_edge(self, vertex1: int, vertex2: int):
+        try:
+            self.list_[vertex1].index(vertex2)
+            print("Edge (%d,%d) is already present" % (vertex1, vertex2))
+        except ValueError:
+            self.list_[vertex1].append(vertex2)
+
+    def add_undirected_edge(self, vertex1: int, vertex2: int):
+        self.add_directed_edge(vertex1, vertex2)
+        self.add_directed_edge(vertex2, vertex1)
+
