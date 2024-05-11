b40cade48e9410a776b1246d06940b4c9353770e
frandosis <frandosis@hotmail.com>
2024-05-11 15:02:07 +0200

    Add ability to add edges to the matrix.

--- src/datastructures/non_linear_datastructures/graph/adjacency_matrix.py
+++ src/datastructures/non_linear_datastructures/graph/adjacency_matrix.py
@@ -3,3 +3,10 @@ class AdjacencyMatrix:
     def __init__(self, total_vertices):
         self.total_vertices = total_vertices
         self.matrix = [[0] * total_vertices for _ in range(total_vertices)]
+
+    def add_edge(self, vertex1, vertex2):
+        if vertex1 == vertex2:
+            print("Same vertex %d and %d" % (vertex1, vertex2))
+        self.matrix[vertex1][vertex2] = 1
+        self.matrix[vertex2][vertex1] = 1
+
