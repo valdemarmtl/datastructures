4d9ed687a9ec27865c5ef95115ed33a42e9d7c8c
frandosis <frandosis@hotmail.com>
2024-05-12 07:07:17 +0200

    Add test for deleting based on index.

--- test/linked_list_tests/singly_linked_list_test.py
+++ test/linked_list_tests/singly_linked_list_test.py
@@ -43,3 +43,12 @@ class TestSinglyLinkedList:
         linked_list.delete_node(2)
         node = linked_list.get_node(1)
         assert node.data == 3
+
+    def test_delete_node_at_pos(self):
+        linked_list = SinglyLinkedList()
+        linked_list.append(1)
+        linked_list.append(2)
+        linked_list.append(3)
+        linked_list.delete_node_at_pos(1)
+        node = linked_list.get_node(1)
+        assert node.data == 3
