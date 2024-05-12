cb4ea09cfe4fae1d2e0d0a11de48fe83d4106fc9
frandosis <frandosis@hotmail.com>
2024-05-12 11:15:17 +0200

    Add method for append to the end of the list using tail.

--- src/datastructures/linear_datastructures/linked_list/doubly_linked_list.py
+++ src/datastructures/linear_datastructures/linked_list/doubly_linked_list.py
@@ -13,3 +13,17 @@ class DoublyLinkedList:
         self.head: Optional[Node] = None
         self.tail: Optional[Node] = None
 
+    def append(self, data: object):
+        if self.tail is None:
+            self._add_first_node(data)
+            return
+        new_node = Node(data)
+        self.tail.next = new_node
+        new_node.prev = self.tail
+        self.tail = new_node
+
+    def _add_first_node(self, data: object):
+        new_node = Node(data)
+        self.head = new_node
+        self.tail = new_node
+
