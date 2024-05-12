4ea59e0bb4bce6ff6a9be2a00906cd031d9b51a0
frandosis <frandosis@hotmail.com>
2024-05-12 06:47:42 +0200

    Add method for inserting at a given index position.

--- src/datastructures/linear_datastructures/linked_list/singly_linked_list.py
+++ src/datastructures/linear_datastructures/linked_list/singly_linked_list.py
@@ -26,3 +26,18 @@ class SinglyLinkedList:
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
+
+    def insert_node_at_pos(self, pos: int, data: object):
+        if self.head is None or pos == 0:
+            self.prepend(data)
+            return
+        new_node = Node(data)
+        current_node = self.head
+        count = 0
+        while current_node and count != pos:
+            prev_node = current_node
+            current_node = current_node.next
+            count += 1
+        prev_node.next = new_node
+        new_node.next = current_node
+
