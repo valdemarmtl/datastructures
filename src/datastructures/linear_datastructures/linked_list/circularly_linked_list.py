00238262fa372fe76d58048a9e20bb93822cd0c8
frandosis <frandosis@hotmail.com>
2024-05-13 08:27:40 +0200

    Add method for inserting at the beginning of the linked list.

--- src/datastructures/linear_datastructures/linked_list/circularly_linked_list.py
+++ src/datastructures/linear_datastructures/linked_list/circularly_linked_list.py
@@ -24,3 +24,16 @@ class CircularlyLinkedList:
         new_node.next = self.head
         self.size += 1
 
+    def prepend(self, data: object):
+        if self.head is None:
+            return self._add_first_node(data)
+
+        new_node = Node(data)
+        current_node = self.head
+        new_node.next = self.head
+        while current_node.next != self.head:
+            current_node = current_node.next
+        current_node.next = new_node
+        self.head = new_node
+        self.size += 1
+
