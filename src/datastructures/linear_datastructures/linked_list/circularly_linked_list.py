52af8363644397aff0d08dabffc80d370b1415b3
frandosis <frandosis@hotmail.com>
2024-05-13 08:28:20 +0200

    Add method for inserting at a given index and add out-of-bounds protection.

--- src/datastructures/linear_datastructures/linked_list/circularly_linked_list.py
+++ src/datastructures/linear_datastructures/linked_list/circularly_linked_list.py
@@ -37,3 +37,20 @@ class CircularlyLinkedList:
         self.head = new_node
         self.size += 1
 
+    def insert_node_index(self, index: int, data: object):
+        if index >= self.size:
+            raise IndexError("Index out of range")
+        if self.head is None or index == 0:
+            return self.prepend(data)
+
+        new_node = Node(data)
+        current_node = self.head
+        count = 0
+        while current_node and count != index:
+            prev_node = current_node
+            current_node = current_node.next
+            count += 1
+        prev_node.next = new_node
+        new_node.next = current_node
+        self.size += 1
+
