2fa6914936f96b34c5a917b06fc66a4ecf62e480
frandosis <frandosis@hotmail.com>
2024-05-16 07:39:38 +0200

    Add test for deleting a node based on a given index.

--- test/linked_list_tests/circularly_linked_list_test.py
+++ test/linked_list_tests/circularly_linked_list_test.py
@@ -92,3 +92,14 @@ class TestCircularlyLinkedList:
         assert circular_linked_list.head.next == circular_linked_list.head
         assert circular_linked_list.size == 1
 
+    def test_delete_node_index(self):
+        circular_linked_list = CircularlyLinkedList()
+        circular_linked_list.append(1)
+        circular_linked_list.append(2)
+        circular_linked_list.append(3)
+        circular_linked_list.delete_node_index(1)
+        assert circular_linked_list.head.data == 1
+        assert circular_linked_list.head.next.data == 3
+        assert circular_linked_list.head.next.next == circular_linked_list.head
+        assert circular_linked_list.size == 2
+
