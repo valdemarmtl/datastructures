4219e924621d00c61c10db8502a71a226145e072
frandosis <frandosis@hotmail.com>
2024-05-16 07:36:36 +0200

    Add test case for deleting the head of the list using key.

--- test/linked_list_tests/circularly_linked_list_test.py
+++ test/linked_list_tests/circularly_linked_list_test.py
@@ -60,3 +60,14 @@ class TestCircularlyLinkedList:
         assert circular_linked_list.head.next.next == circular_linked_list.head
         assert circular_linked_list.size == 2
 
+    def test_delete_node__head(self):
+        circular_linked_list = CircularlyLinkedList()
+        circular_linked_list.append(1)
+        circular_linked_list.append(2)
+        circular_linked_list.append(3)
+        circular_linked_list.delete_node(1)
+        assert circular_linked_list.head.data == 2
+        assert circular_linked_list.head.next.data == 3
+        assert circular_linked_list.head.next.next == circular_linked_list.head
+        assert circular_linked_list.size == 2
+
