f61f61130a8bf64cdfe3f0ebe24b987a5c98898c
frandosis <frandosis@hotmail.com>
2024-05-16 07:35:53 +0200

    Add test for deleting a node based on key.

--- test/linked_list_tests/circularly_linked_list_test.py
+++ test/linked_list_tests/circularly_linked_list_test.py
@@ -49,3 +49,14 @@ class TestCircularlyLinkedList:
         assert circular_linked_list.head.next.next.next == circular_linked_list.head
         assert circular_linked_list.size == 3
 
+    def test_delete_node(self):
+        circular_linked_list = CircularlyLinkedList()
+        circular_linked_list.append(1)
+        circular_linked_list.append(2)
+        circular_linked_list.append(3)
+        circular_linked_list.delete_node(2)
+        assert circular_linked_list.head.data == 1
+        assert circular_linked_list.head.next.data == 3
+        assert circular_linked_list.head.next.next == circular_linked_list.head
+        assert circular_linked_list.size == 2
+
