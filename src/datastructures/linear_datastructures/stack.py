44ae0520d47680687fa830fc9b22375c753e4a39
frandosis <frandosis@hotmail.com>
2024-05-17 08:09:57 +0200

    Add pop method, which can remove head default or any other given index.

--- src/datastructures/linear_datastructures/stack.py
+++ src/datastructures/linear_datastructures/stack.py
@@ -9,3 +9,10 @@ class Stack:
     def push(self, data: object):
         self.list_.prepend(data)
 
+    def pop(self, index: int = 0) -> object:
+        if self.is_empty():
+            raise ValueError("Stack is empty")
+        data = self.list_.get_index(index, data_only=True)
+        self.list_.delete_node_index(index)
+        return data
+
