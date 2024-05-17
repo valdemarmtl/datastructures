936ed0e46c2c3868b1633b2d5f0c98233eefb54e
frandosis <frandosis@hotmail.com>
2024-05-17 08:14:07 +0200

    Add test for poping using an index.

--- test/linear_datastructures/stack_test.py
+++ test/linear_datastructures/stack_test.py
@@ -29,3 +29,13 @@ class TestStack:
         assert stack.pop() == 1
         assert len(stack) == 0
 
+    def test_pop_index(self):
+        stack = Stack()
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        assert stack.pop(1) == 2
+        assert stack.pop(0) == 3
+        assert stack.pop(0) == 1
+        assert len(stack) == 0
+
