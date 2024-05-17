86c093a5b868f8c6d91db92d159d1d7b6ab685d8
frandosis <frandosis@hotmail.com>
2024-05-17 08:13:19 +0200

    Add test for the push method.

--- test/linear_datastructures/stack_test.py
+++ test/linear_datastructures/stack_test.py
@@ -9,3 +9,13 @@ class TestStack:
         assert len(stack) == 0
         assert isinstance(stack.list_, SinglyLinkedList)
 
+    def test_push(self):
+        stack = Stack()
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        assert len(stack) == 3
+        assert stack.get(0) == 3
+        assert stack.get(1) == 2
+        assert stack.get(2) == 1
+
