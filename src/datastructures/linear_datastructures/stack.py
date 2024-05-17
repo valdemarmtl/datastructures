29ff7ac49d3fde6c84e7bacbe0b07e4e43dbe6de
frandosis <frandosis@hotmail.com>
2024-05-17 08:08:46 +0200

    Add initial class with initializer.

---
+++ src/datastructures/linear_datastructures/stack.py
@@ -0,0 +1,8 @@
+
+from datastructures.linear_datastructures.linked_list.singly_linked_list import SinglyLinkedList
+
+
+class Stack:
+    def __init__(self):
+        self.list_ = SinglyLinkedList()
+
