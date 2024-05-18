3cfc4b2e0896dfb2fcbd345e5ac0f5b5f9b4905e
frandosis <frandosis@hotmail.com>
2024-05-18 09:40:28 +0200

    Add test for inserting an undirected edge using the simple wrapper.

--- test/non_linear_datastructures/graph/adjacency_list_test.py
+++ test/non_linear_datastructures/graph/adjacency_list_test.py
@@ -11,3 +11,13 @@ class TestAdjacencyList:
         assert adj_list.total_vertices == total_vertices
         assert len(adj_list.list_) == len(adj_list.list_)
 
+    def test_add_edge(self):
+        total_vertices = 5
+        list_of_edges = [(0, 1), (1, 3), (2, 3), (3, 3), (3, 4), (4, 1)]
+        adj_list = AdjacencyList(total_vertices)
+        for edge in list_of_edges:
+            adj_list.add_edge(*edge)
+
+        assert adj_list.list_ == [[1], [0, 3, 4], [3], [1, 2, 3, 4], [3, 1]]
+        assert adj_list.total_vertices == total_vertices
+
