ffc1ad093a7bf0227dc04130552daff91fdfb9c2
frandosis <frandosis@hotmail.com>
2024-05-18 09:41:07 +0200

    Add test for adding a directed edge into the graph.

--- test/non_linear_datastructures/graph/adjacency_list_test.py
+++ test/non_linear_datastructures/graph/adjacency_list_test.py
@@ -32,3 +32,13 @@ class TestAdjacencyList:
         assert adj_list.list_ == [[1], [0, 3, 4], [3], [1, 2, 3, 4], [3, 1]]
         assert adj_list.total_vertices == total_vertices
 
+    def test_add_directed_edge(self):
+        total_vertices = 5
+        list_of_edges = [(0, 1), (1, 3), (2, 3), (3, 3), (3, 4), (4, 1)]
+        adj_list = AdjacencyList(total_vertices)
+        for edge in list_of_edges:
+            adj_list.add_directed_edge(*edge)
+
+        assert adj_list.list_ == [[1], [3], [3], [3, 4], [1]]
+        assert adj_list.total_vertices == total_vertices
+
