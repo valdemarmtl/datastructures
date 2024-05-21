2673e94ee74d6d396289483c6aa943d3288fcfb2
frandosis <frandosis@hotmail.com>
2024-05-21 07:47:42 +0200

    Add initial imports and class for the queue.

---
+++ src/datastructures/linear_datastructures/queue.py
@@ -0,0 +1,6 @@
+from datastructures.linear_datastructures.linked_list.singly_linked_list import SinglyLinkedList
+
+
+class Queue:
+    def __init__(self) -> None:
+        self.list_ = SinglyLinkedList()
